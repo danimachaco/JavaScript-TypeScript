6008debde14d18443152de59c93c7f30b3753966
daniel <danimachacov@gmail.com>
2026-03-02 11:16:11 +0100

    Correccion primeros ejercicios de funciones y arrays en JS

--- practicas_parte2_trimestre/arrays/ej_arrays_5.js
+++ practicas_parte2_trimestre/arrays/ej_arrays_5.js
@@ -1,19 +1,20 @@
 const salaDeCine = 
     [
-    [1 , 1 , 0 , 0 , 0] ,
+    [1 , 1 , 1 , 0 , 0] ,
     [1 , 1 , 1 , 0 , 1] ,
     [0 , 0 , 0 , 1 , 1] ,
     [1 , 1 , 1 , 1 , 1] 
     ];
 
-    let vLibre = 0;
-
-    let encontrado = false;
+let vLibre = 0;
+let i = 0;
+let j = 0;
+let encontrado = false;
 
 //Recorremos todos los asientos de las filas de la sala
-for (let i = 0; i < salaDeCine.length; i++){
+while (i < salaDeCine[0].length && encontrado == false){
 
-    for (let j = 0; j <= salaDeCine.length; j++){
+    while (j < salaDeCine[0].length && encontrado == false){
 
         if (salaDeCine [i][j] == 0){  //Detecta si el asiento esta libre y si es asi se incrementa el valor de la variable
 
@@ -32,15 +33,21 @@ for (let i = 0; i < salaDeCine.length; i++){
 
         }
 
-        if (encontrado == true){   //Cuando ya sabemos que hay 3 asientos libres, imprimimos en la fila en la que se encuentran y salimos del bucle
+        j++;
 
-            console.log ("Asientos disponibles en la fila: " + i)
-
-            i = salaDeCine.length;
-
-            j = salaDeCine.length;
-
-        }
     }
+    
+    i++;
+    
+    j = 0;
+
+}
+
+let fila = i -1;
+
+if (encontrado == true){   //Cuando ya sabemos que hay 3 asientos libres, imprimimos en la fila en la que se encuentran y salimos del bucle
+
+    console.log ("Asientos disponibles en la fila: " + fila);
+
 }
 
